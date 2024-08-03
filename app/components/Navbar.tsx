91092897174d869eba502d70fa6cc71774882dd1
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 00:17:08 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,8 +3,11 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-    <header className='bg-slate-500 '>
+    <header className='bg-slate-500 w-full'>
+        <section className='wf
+        '>
 
+        </section>
     </header>
     </>
   )
