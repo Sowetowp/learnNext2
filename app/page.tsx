9651ca0fef8b05c44e61db702f7882aeee3bb0e3
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-03 23:53:52 +0100

    auto-commit

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main><h1>hello world</h1></main>
+    <main><h1>hello world</h1><img src={Image} alt="" /></main>
   );
 }
