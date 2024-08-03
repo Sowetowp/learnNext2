8e330994065444d3cf2c77f29b829ca6193cbf1c
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 00:54:13 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -15,7 +15,7 @@ const Navbar = () => {
 
     return (
         <>
-            <header className=`bg-slate-500 w-full ${scrollHeader ? "fixed"}>
+            <header className=`bg-slate-500 w-full ${scrollHeader ? "fixed" : }>
                 <section className='w-full max-w-7xl py-7 px-1 md:px-16 flex items-center justify-between'>
                     <h1 className='text-white text-xl leading-none tracking-widest'>SPERE</h1>
                     <div className='flex gap-8 items-center'>
