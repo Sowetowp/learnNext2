0d0190394992c47af57741bd8d12e9d106f15c61
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 00:35:40 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
                 <section className='w-full max-w-7xl py-7 px-1 md:px-16'>
                     <h1 className='text-white text-xl leading-none tracking-widest'>SPERE</h1>
                     <div className='flex gap-8'>
-                        <a className='text-white text-[0.77rem] font-semibold'>HOME</a>
+                        <a className='text-white text-[0.8rem] font-semibold'>HOME</a>
                         <a className='text-white text-xs font-semibold' href="">ABOUT</a>
                         <a className='text-white text-xs font-semibold' href="">SERVICES</a>
                         <a className='text-white text-xs font-semibold' href="">TEAM</a>
