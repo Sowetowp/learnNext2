6968f7d34d9cc56596737678b703a233bc557b49
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 00:19:33 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ const Navbar = () => {
     return (
         <>
             <header className='bg-slate-500 w-full'>
-                <section className='w-full max-w-7xl'>
+                <section className='w-full max-w-7xl py-4 px-1 md:'>
                     <h1>SPERE</h1>
                 </section>
             </header>
