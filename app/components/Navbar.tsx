687a11aee2cce7f7d49f9ebe745930851e4c0863
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 00:19:00 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,15 +1,15 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <>
-    <header className='bg-slate-500 w-full'>
-        <section className='w-full max-w-7xl'>
-<h1>SPERE</h1>
-        </section>
-    </header>
-    </>
-  )
+    return (
+        <>
+            <header className='bg-slate-500 w-full'>
+                <section className='w-full max-w-7xl'>
+                    <h1>SPERE</h1>
+                </section>
+            </header>
+        </>
+    )
 }
 
 export default Navbar
