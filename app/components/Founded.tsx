00794946b88c9573989cd396d81e8b596de935d5
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 15:55:34 +0100

    auto-commit

---
+++ app/components/Founded.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Founded = () => {
+  return (
+    <div>Founded</div>
+  )
+}
+
+export default Founded
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Carousel from "./components/Carousel";
 import Expert from "./components/Expert";
+import Founded from "./components/Founded";
 
 export default function Home() {
   return (
     <>
       <Carousel />
       <Expert />
+      <Founded/>
     </>
   );
 }
