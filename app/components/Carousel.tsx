27107174a5211ee0856b5bdebf78adf3b08870cf
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 05:22:12 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -4,8 +4,6 @@ import Navbar from './Navbar'
 
 const Carousel = () => {
     const [currentIndex, setCurrentIndex] = useState(0)
-    const [next, setNext] = useState(true)
-    const [prev, setPrev] = useState(false)
     const slides = [
         '/hero-4.jpg',
         '/hero-5.jpeg',
@@ -25,19 +23,15 @@ const Carousel = () => {
             return "100%"
         }
     }
-useState(()=>{
-    if(!next){
+
+    const handlePrev = () => {
         setCurrentIndex((prevIndex) =>
             prevIndex === 0 ? slides.length - 1 : prevIndex - 1
         );
-    }
-},[next])
-    const handlePrev = () => {
     };
 
     console.log(currentIndex)
     const handleNext = () => {
-        setNext(false)
         setCurrentIndex((prevIndex) =>
             prevIndex === slides.length - 1 ? 0 : prevIndex + 1
         );
@@ -47,7 +41,7 @@ useState(()=>{
         <>
             <main className='h-screen bg-black m-auto w-full max-w-7xl relative'>
                 {slides.map((slide, index) => (
-                    <div key={index} className={`h-full w-full absolute ${currentIndex === index ? "z-[11]" : currentIndex === slides.length - 1 && index === 0 && prev ? "-z-10" : index === slides.length - 1 && currentIndex === 0 && next ? "-z-10" : "z-10"} transition-transform duration-500`} style={{ transform: `translateX(${position(index)})` }}>
+                    <div key={index} className={`h-full w-full absolute ${currentIndex === index ? "z-[11]" : currentIndex === slides.length - 1 && index === 0 ? "-z-10" : index === slides.length - 1 && currentIndex !== 0 ? "-z-10" : "z-10"} transition-transform duration-500`} style={{ transform: `translateX(${position(index)})` }}>
                         <div className='h-full w-full absolute text-center bg-[#00000091] flex justify-center items-center'>
                             <span className='pt-20'>
                                 <p className='text-white font-bold text-5xl'>Talented Consultants</p>
@@ -60,8 +54,8 @@ useState(()=>{
                 ))}
                 <Navbar />
             </main>
-            <button className='border bg-black  p-14' onClick={()=>setNext(false)}>kk</button>
-            <button className='border bg-red-500  p-14' onClick={()=>setPrev(false)}>kk</button>
+            <button className='border bg-black  p-14' onClick={handleNext}>kk</button>
+            <button className='border bg-red-500  p-14' onClick={handlePrev}>kk</button>
         </>
     )
 }
