5753d3ddfc99f5208bf64291f192c333de70a8be
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 14:40:18 +0100

    auto-commit

--- app/components/Expert.tsx
+++ app/components/Expert.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Expert = () => {
     return (
         <>
-            <section className='w-full max-w-7xl m-auto bg-white py-10'>
-                <h2>We are an expert consulting firm.</h2>
+            <section className='w-full max-w-7xl m-auto bg-white py-20'>
+                <h2 className='font-bold'>We are an expert consulting firm.</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nemo libero debitis vitae. Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.</p>
             </section>
         </>
