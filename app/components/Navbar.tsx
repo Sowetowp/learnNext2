ea480ca39ee93a3897218918193a9543122d6273
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 06:33:11 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -45,6 +45,26 @@ const Navbar = () => {
                         <a className='text-white text-[0.8rem] font-semibold' href="">BLOG</a>
                         <a className='text-white text-[0.8rem] font-semibold' href="">CONTACT</a>
                     </div>
+                    <div>
+                    /* From Uiverse.io by Shubh0408 */ 
+<label>
+  <div
+    class="w-9 h-10 cursor-pointer flex flex-col items-center justify-center"
+  >
+    <input class="hidden peer" type="checkbox" />
+    <div
+      class="w-[50%] h-[2px] bg-black rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] peer-checked:rotate-[-45deg]"
+    ></div>
+    <div
+      class="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-center peer-checked:hidden"
+    ></div>
+    <div
+      class="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-left -translate-y-[0.45rem] peer-checked:rotate-[45deg]"
+    ></div>
+  </div>
+</label>
+
+                    </div>
                 </section>
             </header>
         </>
