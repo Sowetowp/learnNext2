6ab55d0c778aa494e2a82ac4f58b0107213d3608
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 19:19:41 +0100

    auto-commit

--- app/components/Award.tsx
+++ app/components/Award.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Award = () => {
   return (
     <>
-      <section>
+      <section className=''>
         <div className="parallax"></div>
         <div>kkk</div>
       </section>
