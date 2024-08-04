e5623078b5df59c8f76bc18fd6c5dc5141a0b7f8
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 16:01:15 +0100

    auto-commit

--- app/components/Founded.tsx
+++ app/components/Founded.tsx
@@ -4,7 +4,8 @@ const Founded = () => {
   return (
     <>
     <section className='w-full max-w-7xl m-auto bg-stone-50 py-28'>
-        <div ></div>
+        <div className='w-1/2'>
+        </div>
     </section>
     </>
   )
