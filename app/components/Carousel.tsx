88353682c37c99d208cfc7155157d979817ed1d5
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 02:16:58 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -2,24 +2,23 @@ import React from 'react'
 import Navbar from './Navbar'
 
 const Carousel = () => {
-  return (
-    <>
-    <main className='h-screen bg-black w-full max-w-7xl relative'>
-        <div className='h-full w-full absolute z-10'>
-            <div className='h-full w-full absolute bg-[#00000091] flex justify-center items-center'>
-                <span>
-                    
-                </span>
-                <p className='text-white font-bold text-5xl'>Talented Consultants</p>
-                <p>With over 20 years experience helping business to find comprehensive solutions</p>
-                <a href="">OUR SERVICES</a>
-            </div>
-            <img className='h-full w-full object-cover' src="/hero-4.jpg" alt="" />
-        </div>
-        <Navbar/>
-    </main>
-    </>
-  )
+    return (
+        <>
+            <main className='h-screen bg-black w-full max-w-7xl relative'>
+                <div className='h-full w-full absolute z-10'>
+                    <div className='h-full w-full absolute bg-[#00000091] flex justify-center items-center'>
+                        <span>
+                            <p className='text-white font-bold text-5xl'>Talented Consultants</p>
+                            <p>With over 20 years experience helping business to find comprehensive solutions</p>
+                            <a href="">OUR SERVICES</a>
+                        </span>
+                    </div>
+                    <img className='h-full w-full object-cover' src="/hero-4.jpg" alt="" />
+                </div>
+                <Navbar />
+            </main>
+        </>
+    )
 }
 
 export default Carousel
