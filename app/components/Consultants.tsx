ad2020d924badf2879e927585e3b3824954098cc
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 20:57:25 +0100

    auto-commit

--- app/components/Consultants.tsx
+++ app/components/Consultants.tsx
@@ -13,8 +13,8 @@ const Consultants = () => {
                 <h2 className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]'>Our expert consultants</h2>
                 <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text.</p>
                 <div className='w-full px-10 flex flex-wrap'>
-                    {}
-                    <div className='xl:w-1/4 md:w-1/2 w-full text-center mt-14 px-3'>
+                    {exp.map((ex, index)=>(
+                    <div key={index} className='xl:w-1/4 md:w-1/2 w-full text-center mt-14 px-3'>
                         <div className='relative'>
                             <div className='h-[19rem] flex items-center justify-center gap-3 w-full opacity-0 hover:opacity-100 transition-opacity duration-500 z-10 absolute bg-blue-700'>
                                 <a><svg className='w-3' viewBox="-5 0 20 20" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="#fff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>facebook [#176]</title> <desc>Created with Sketch.</desc> <defs> </defs> <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"> <g id="Dribbble-Light-Preview" transform="translate(-385.000000, -7399.000000)" fill="#fff"> <g id="icons" transform="translate(56.000000, 160.000000)"> <path d="M335.821282,7259 L335.821282,7250 L338.553693,7250 L339,7246 L335.821282,7246 L335.821282,7244.052 C335.821282,7243.022 335.847593,7242 337.286884,7242 L338.744689,7242 L338.744689,7239.14 C338.744689,7239.097 337.492497,7239 336.225687,7239 C333.580004,7239 331.923407,7240.657 331.923407,7243.7 L331.923407,7246 L329,7246 L329,7250 L331.923407,7250 L331.923407,7259 L335.821282,7259 Z" id="facebook-[#176]"> </path> </g> </g> </g> </g></svg></a>
@@ -26,6 +26,7 @@ const Consultants = () => {
                         <p className='font-medium text-xs tracking-wider mt-5 text-center'></p>
                         <p className='text-[0.8rem] mt-1 text-gray-500'></p>
                     </div>
+                    ))}
                 </div>
             </section>
         </>
