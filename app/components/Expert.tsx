4bc7f73ded5ff3089c4f7cff7c67b4dd20178707
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 15:04:44 +0100

    auto-commit

--- app/components/Expert.tsx
+++ app/components/Expert.tsx
@@ -8,7 +8,9 @@ const Expert = () => {
                 <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-1/2 text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nemo libero debitis vitae. Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.</p>
                 <div className='w-full'>
                     <div className='w-1/3'>
-                    
+                    <span>
+                        
+                    </span>
                     </div>
                 </div>
             </section>
