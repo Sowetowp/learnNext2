aa9f32aecd6cec9ee818b8344232c19b20adbefa
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 03:53:43 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,8 +1,9 @@
+"use client";
 import React, { useState } from 'react'
 import Navbar from './Navbar'
 
 const Carousel = () => {
-    const [currentIndex, setCurrentIndex]=useState(0)
+    const [currentIndex, setCurrentIndex] = useState(0)
     const slides = [
         '/hero-4.jpg',
         '/hero-5.jpeg',
@@ -13,7 +14,7 @@ const Carousel = () => {
         <>
             <main className='h-screen bg-black w-full max-w-7xl relative'>
                 {slides.map((slide, index) => (
-                    <div key={index} className='h-full w-full absolute z-10 transition-transform duration-500' style={{ transform: `translateX(-${ 100}%)` }}>
+                    <div key={index} className='h-full w-full absolute z-10 transition-transform duration-500' style={{ transform: `translateX(-${100}%)` }}>
                         <div className='h-full w-full absolute text-center bg-[#00000091] flex justify-center items-center'>
                             <span className='pt-20'>
                                 <p className='text-white font-bold text-5xl'>Talented Consultants</p>
