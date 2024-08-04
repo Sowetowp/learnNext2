d202c72e0f6523fa09cd491303c7443f07e6b4f6
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 12:47:15 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -55,7 +55,7 @@ const Navbar = () => {
                         <label>
                             <div className="w-9 h-10 cursor-pointer flex flex-col items-center justify-center">
                                 <input onChange={(e) => handleMenuChange(e)} className="hidden peer" type="checkbox" />
-                                <div className="w-[50%] h-[2px] bg-white rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] peer-checked:rotate-[-45deg]" ></div>
+                                <div className={`w-[50%] h-[2px] bg-white rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] peer-checked:rotate-[-45deg]`} ></div>
                                 <div className="w-[50%] h-[2px] bg-white rounded-md transition-all duration-300 origin-center peer-checked:hidden" ></div>
                                 <div className="w-[50%] h-[2px] bg-white rounded-md transition-all duration-300 origin-left -translate-y-[0.45rem] peer-checked:rotate-[45deg]" ></div>
                             </div>
