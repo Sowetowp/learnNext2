e03e0fbf2e88f82b3bf1420cba1d88dccd4c1a07
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 02:11:21 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -8,7 +8,7 @@ const Carousel = () => {
         <div className='h-full w-full absolute z-10'>
             <div className='h-full w-full absolute bg-[#00000091]'>
                 <p>Talented Consultants</p>
-                <p>with over 20 years of helping companies to find</p>
+                <p>with over 20 years experience helping business to find</p>
             </div>
             <img className='h-full w-full object-cover' src="/hero-4.jpg" alt="" />
         </div>
