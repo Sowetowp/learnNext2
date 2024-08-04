e43bef24b0f69f93c690114447de5139962c99f5
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 16:16:22 +0100

    auto-commit

--- app/components/Founded.tsx
+++ app/components/Founded.tsx
@@ -5,7 +5,7 @@ const Founded = () => {
         <>
             <section className='w-full flex items-center flex-wrap max-w-7xl m-auto bg-stone-50 px-3 md:px-16 py-28'>
                 <div className='md:w-1/2 w-full'>
-                    <h2 className='font-bold w-[80%] text-[1.9rem] mb-6'>Founded with the goal to serve expertise</h2>
+                    <h2 className='font-bold md:w-[80%] text-[1.9rem] mb-6'>Founded with the goal to serve expertise</h2>
                     <p className='text-[1.1rem] border-b pb-1 w-fit font-light text-gray-500 mt-1'>- Template is easy to setup and run</p>
                     <p className='text-[1.1rem] border-b pb-1 w-fit font-light text-gray-500 mt-1'>- Free support and lifetime updates</p>
                     <p className='text-[1.1rem] border-b pb-1 w-fit font-light text-gray-500 mt-1'>- Modern and unique design</p>
