46222ba39fb533b91f29260edae766dd5f4293e6
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 12:48:53 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -55,9 +55,9 @@ const Navbar = () => {
                         <label>
                             <div className="w-9 h-10 cursor-pointer flex flex-col items-center justify-center">
                                 <input onChange={(e) => handleMenuChange(e)} className="hidden peer" type="checkbox" />
-                                <div className={`w-[50%] h-[2px] ${scrollHeader?"text-black":"text-white"} rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] peer-checked:rotate-[-45deg]`} ></div>
-                                <div className={`w-[50%] h-[2px] ${scrollHeader?"text-black":"text-white"} rounded-md transition-all duration-300 origin-center peer-checked:hidden`} ></div>
-                                <div className={`w-[50%] h-[2px] ${scrollHeader?"text-black":"text-white"} rounded-md transition-all duration-300 origin-left -translate-y-[0.45rem] peer-checked:rotate-[45deg]`} ></div>
+                                <div className={`w-[50%] h-[2px] ${scrollHeader?"bg-black":"bg-white"} rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] peer-checked:rotate-[-45deg]`} ></div>
+                                <div className={`w-[50%] h-[2px] ${scrollHeader?"bg-black":"bg-white"} rounded-md transition-all duration-300 origin-center peer-checked:hidden`} ></div>
+                                <div className={`w-[50%] h-[2px] ${scrollHeader?"bg-black":"bg-white"} rounded-md transition-all duration-300 origin-left -translate-y-[0.45rem] peer-checked:rotate-[45deg]`} ></div>
                             </div>
                         </label>
                     </div>
