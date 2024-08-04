364b4b1d5c6c1e7efa60916023b1b4b870f629de
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 12:09:03 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -62,7 +62,7 @@ const Navbar = () => {
                         </label>
                     </div>
                 </section>
-                <section className={`w-full m-auto max-w-7xl md:hidden flex-col ${menu ? "flex opacity-100" : "hidden opacity-0"} transitio`}>
+                <section className={`w-full m-auto max-w-7xl md:hidden flex-col ${menu ? "flex opacity-100" : "hidden opacity-0"} transition-opacity`}>
                     <a className='text-white text-[0.8rem] font-semibold py-2 text-center bg-[#000000de]'>HOME</a>
                     <a className='text-white text-[0.8rem] font-semibold py-2 text-center bg-[#000000de]' href="">ABOUT</a>
                     <a className='text-white text-[0.8rem] font-semibold py-2 text-center bg-[#000000de]' href="">SERVICES</a>
