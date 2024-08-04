87506e35a0a2fb6b1747f1cfc83154d805b32695
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 22:43:50 +0100

    auto-commit

--- app/components/Happy.tsx
+++ app/components/Happy.tsx
@@ -7,7 +7,7 @@ const Happy = () => {
     const [count1, setCount1] = useState(0)
     const [count2, setCount2] = useState(0)
     const [count3, setCount3] = useState(0)
-    console.log(scrollHeader)
+
     useEffect(() => {
         const interval = setInterval(() => {
             if (scrollHeader && count < 1542) {
