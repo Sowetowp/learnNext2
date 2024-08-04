a2d843608a33285961edfcb6c903b391ffa563fd
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 14:44:56 +0100

    auto-commit

--- app/components/Expert.tsx
+++ app/components/Expert.tsx
@@ -4,8 +4,8 @@ const Expert = () => {
     return (
         <>
             <section className='w-full max-w-7xl m-auto bg-white py-20'>
-                <h2 className='font-bold text-[1.6rem]'>We are an expert consulting firm.</h2>
-                <p className='mt-7 text-sm text-gray-500 w-1/2 text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nemo libero debitis vitae. Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.</p>
+                <h2 className='font-bold text-[1.6rem] text-center'>We are an expert consulting firm.</h2>
+                <p className='mt-7 m-auto text-sm text-gray-500 w-1/2 text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nemo libero debitis vitae. Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.</p>
             </section>
         </>
     )
