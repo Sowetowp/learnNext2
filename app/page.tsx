9de1dee6c9e27bda5a063d916fe2837ebce1a4cd
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 21:29:18 +0100

    auto-commit

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Founded from "./components/Founded";
 import Award from "./components/Award";
 import Services from "./components/Services";
 import Consultants from "./components/Consultants";
+import Happy from "./components/Happy";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <Award />
       <Services />
       <Consultants />
+      <Happy/>
     </>
   );
 }
