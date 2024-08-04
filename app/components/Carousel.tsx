ac1cdf50de7015868b976f84d488fea41611410b
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 13:47:52 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -6,9 +6,9 @@ const Carousel = () => {
     const [currentIndex, setCurrentIndex] = useState(0)
     const prevIndex = useRef(0)
     const slides = [
-        { img: '/hero-4.jpg', title: "Talented Consultants", details: "With over 20 years experience helping business to find comprehensive solutions", url:"", btn: "OUR SERVICES" },
-        { img: '/hero-5.jpeg', title: "Qualified Professionals", details: "We have spent thousands of hours to learn technology for solving problems", url:"", btn: "ABOUT US" },
-        { img: '/hero-6.jpg', title: "Returns Guaranteed", details: "We have helped hundreds of companies to grow their business in the competitive world", url:"", btn: "CASE STUDIES" },
+        { img: '/hero-4.jpg', title: "Talented Consultants", details: "With over 20 years experience helping business to find comprehensive solutions", url: "", btn: "OUR SERVICES" },
+        { img: '/hero-5.jpeg', title: "Qualified Professionals", details: "We have spent thousands of hours to learn technology for solving problems", url: "", btn: "ABOUT US" },
+        { img: '/hero-6.jpg', title: "Returns Guaranteed", details: "We have helped hundreds of companies to grow their business in the competitive world", url: "", btn: "CASE STUDIES" },
     ];
 
     const position = (index: number) => {
@@ -61,8 +61,9 @@ const Carousel = () => {
                     <svg className='w-7' viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M9.71069 18.2929C10.1012 18.6834 10.7344 18.6834 11.1249 18.2929L16.0123 13.4006C16.7927 12.6195 16.7924 11.3537 16.0117 10.5729L11.1213 5.68254C10.7308 5.29202 10.0976 5.29202 9.70708 5.68254C9.31655 6.07307 9.31655 6.70623 9.70708 7.09676L13.8927 11.2824C14.2833 11.6729 14.2833 12.3061 13.8927 12.6966L9.71069 16.8787C9.32016 17.2692 9.32016 17.9023 9.71069 18.2929Z" fill="#fff"></path> </g></svg>
                 </button>
                 <div className='absolute z-20'>
-                    {}
-                    <div className={`bg-red-500 border-2 border-white w-3 h-3 rounded-full`}></div>
+                    {slides.map((slide, index) => (
+                        <div key={index} className={`bg-red-500 border-2 border-white w-3 h-3 rounded-full`}></div>
+                    ))}
                 </div>
                 <Navbar />
             </main>
