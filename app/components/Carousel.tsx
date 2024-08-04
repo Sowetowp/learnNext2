3d1c83b5606d68d7bca0c35c609a528c78c99f7a
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 03:44:55 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -11,7 +11,7 @@ const Carousel = () => {
     return (
         <>
             <main className='h-screen bg-black w-full max-w-7xl relative'>
-                
+                {slides.map()}
                 <div className='h-full w-full absolute z-10'>
                     <div className='h-full w-full absolute text-center bg-[#00000091] flex justify-center items-center'>
                         <span className='pt-20'>
