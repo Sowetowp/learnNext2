d740bfdc65d1dfce34721debc4b221e395296c9a
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 16:02:56 +0100

    auto-commit

--- app/components/Founded.tsx
+++ app/components/Founded.tsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
 const Founded = () => {
-  return (
-    <>
-    <section className='w-full max-w-7xl m-auto bg-stone-50 py-28'>
-        <div className='w-1/2'>
-        <h2>Founded with the goal to serve expertise</h2>
-        <p>- Template is easy to setup and run</p>
-        <p>- Free support and lifetime updates</p>
-        <p>- Modern and unique design</p>
-        </div>
-    </section>
-    </>
-  )
+    return (
+        <>
+            <section className='w-full max-w-7xl m-auto bg-stone-50 py-28'>
+                <div className='w-1/2'>
+                    <h2>Founded with the goal to serve expertise</h2>
+                    <p>- Template is easy to setup and run</p>
+                    <p>- Free support and lifetime updates</p>
+                    <p>- Modern and unique design</p>
+                </div>
+            </section>
+        </>
+    )
 }
 
 export default Founded
