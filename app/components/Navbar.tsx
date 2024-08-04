0225532a8fd65663e93b9ccad22b2f53b75e5f0e
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 12:29:09 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -39,7 +39,7 @@ const Navbar = () => {
     }
     return (
         <>
-            <header className={`w-full h-fit bg-red-500 ${scrollHeader ? "fixed z-[9999] bg-[#000000f3]" : "relative z-[9999]"} ${menu? "bgm-[#000000f3]":""} md:bg-transparent`}>
+            <header className={`w-full h-fit bg-red-500 ${scrollHeader ? "fixed z-[9999] bg-[#000000f3]" : "relative z-[9999]"} ${menu ? "bgm-[#000000f3]" : ""} md:bg-transparent`}>
                 <section className='w-full m-auto max-w-7xl py-4 md:py-7 px-1 md:px-8 xl:px-16 flex items-center justify-between'>
                     <h1 className='text-white text-xl leading-none tracking-widest'>SPERE</h1>
                     <div className='md:flex gap-8 items-center hidden'>
@@ -54,7 +54,7 @@ const Navbar = () => {
                     <div className='md:hidden'>
                         <label>
                             <div className="w-9 h-10 cursor-pointer flex flex-col items-center justify-center">
-                                <input onChange={(e)=>handleMenuChange(e)} className="hidden peer" type="checkbox" />
+                                <input onChange={(e) => handleMenuChange(e)} className="hidden peer" type="checkbox" />
                                 <div className="w-[50%] h-[2px] bg-white rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] peer-checked:rotate-[-45deg]" ></div>
                                 <div className="w-[50%] h-[2px] bg-white rounded-md transition-all duration-300 origin-center peer-checked:hidden" ></div>
                                 <div className="w-[50%] h-[2px] bg-white rounded-md transition-all duration-300 origin-left -translate-y-[0.45rem] peer-checked:rotate-[45deg]" ></div>
@@ -62,16 +62,16 @@ const Navbar = () => {
                         </label>
                     </div>
                 </section>
-                <section className={`w-full m-auto max-w-7xl md:hidden flex-col ${menu ? "flex" : "flex"} bg-[#000000de] transition-transform duration-500`} style={{transform:`translateX(${menu?"0%":"100%"})`}}>
-                    <a className='text-white text-[0.8rem] font-semibold py-2 text-center'>HOME</a>
-                    <a className='text-white text-[0.8rem] font-semibold py-2 text-center' href="">ABOUT</a>
-                    <a className='text-white text-[0.8rem] font-semibold py-2 text-center' href="">SERVICES</a>
-                    <a className='text-white text-[0.8rem] font-semibold py-2 text-center' href="">TEAM</a>
-                    <a className='text-white text-[0.8rem] font-semibold py-2 text-center' href="">WORK</a>
-                    <a className='text-white text-[0.8rem] font-semibold py-2 text-center' href="">BLOG</a>
-                    <a className='text-white text-[0.8rem] font-semibold py-2 text-center' href="">CONTACT</a>
-                </section>
             </header>
+            <section className={`w-full m-auto max-w-7xl md:hidden flex-col ${menu ? "flex" : "flex"} bg-[#000000de] transition-transform duration-500`} style={{ transform: `translateX(${menu ? "0%" : "100%"})` }}>
+                <a className='text-white text-[0.8rem] font-semibold py-2 text-center'>HOME</a>
+                <a className='text-white text-[0.8rem] font-semibold py-2 text-center' href="">ABOUT</a>
+                <a className='text-white text-[0.8rem] font-semibold py-2 text-center' href="">SERVICES</a>
+                <a className='text-white text-[0.8rem] font-semibold py-2 text-center' href="">TEAM</a>
+                <a className='text-white text-[0.8rem] font-semibold py-2 text-center' href="">WORK</a>
+                <a className='text-white text-[0.8rem] font-semibold py-2 text-center' href="">BLOG</a>
+                <a className='text-white text-[0.8rem] font-semibold py-2 text-center' href="">CONTACT</a>
+            </section>
         </>
     )
 }
