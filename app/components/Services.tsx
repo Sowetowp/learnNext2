224e58a10a96c49a2b183b9c927a80462d071480
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 19:34:58 +0100

    auto-commit

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -18,7 +18,7 @@ const Services = () => {
     return (
         <>
             <section className='w-full max-w-7xl m-auto bg-white py-28'>
-                <h2 className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]'>We are an expert consulting firm.</h2>
+                <h2 className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]'>Our services</h2>
                 <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-1/2 text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nemo libero debitis vitae. Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.</p>
                 <div className='w-full px-10 flex flex-wrap'>
                     {exp.map((ex, index) => (
