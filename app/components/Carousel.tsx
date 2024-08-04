6961d8279e0f687fd25d4554c8c47e5eccb1f4fb
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 04:35:11 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -55,6 +55,7 @@ const Carousel = () => {
                 <Navbar />
             </main>
             <button className='border bg-black  p-14' onClick={handleNext}>kk</button>
+            <button className='border bg-black  p-14' onClick={handleNext}>kk</button>
         </>
     )
 }
