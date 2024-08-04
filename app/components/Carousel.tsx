2e1b2aed475e5df905949011bb2f34c6ba4f3fb3
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 06:14:36 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -9,7 +9,7 @@ const Carousel = () => {
     const slides = [
         {img:'/hero-4.jpg', title: "Talented Consultants", details: "With over 20 years experience helping business to find comprehensive solutions"},
         {img: '/hero-5.jpeg', title:"Qualified Professionals", details: "We have spent thousands of hours to learn technology for solving problems"},
-        '/hero-6.jpg',
+        {img:'/hero-6.jpg', title: ""},
     ];
 
     const position = (index: number) => {
