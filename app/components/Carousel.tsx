f50b3b03619c8db988c3b8c30824b2d3c3725f29
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 01:43:39 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Image from 'next/image'
 
 const Carousel = () => {
   return (
     <>
     <main className='h-screen bg-black w-full max-w-7xl'>
         <div>
-            <Image width={100} height={100} layout="responsive" src="/hero-4.jpg" alt='carousel image 1'/>
+            <img src="/hero" alt="" />
         </div>
         <Navbar/>
     </main>
