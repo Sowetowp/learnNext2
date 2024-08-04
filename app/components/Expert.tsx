1b7465d6b65eb4c1a7cf6323e684002e06659eab
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 15:24:56 +0100

    auto-commit

--- app/components/Expert.tsx
+++ app/components/Expert.tsx
@@ -20,7 +20,7 @@ const Expert = () => {
             <section className='w-full max-w-7xl m-auto bg-white py-28'>
                 <h2 className='font-bold text-[1.6rem] text-center'>We are an expert consulting firm.</h2>
                 <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-1/2 text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nemo libero debitis vitae. Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.</p>
-                <div className='w-full px-10 mt-14'>
+                <div className='w-full px-10 mt-14 flex'>
                     {exp.map((ex, index) => (
                         <div className='w-1/3 md:px-10 text-center mt-14'>
                             <span className='bg-blue-700 inline-block p-6 rounded-full'>{ex.img}</span>
