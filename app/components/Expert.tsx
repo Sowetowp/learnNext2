c752daaa2fbe302881cde4d4ca12b148d982e908
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 15:20:38 +0100

    auto-commit

--- app/components/Expert.tsx
+++ app/components/Expert.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
 const Expert = () => {
-    const exp = ()=>{
+    const exp = [
         
-    }
+    ]
     return (
         <>
             <section className='w-full max-w-7xl m-auto bg-white py-28'>
