2d0b5c6c99cf1bda50ad34789b48b99b2fcea433
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 01:47:55 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -6,7 +6,7 @@ const Carousel = () => {
     <>
     <main className='h-screen bg-black w-full max-w-7xl relative'>
         <div className='h-full w-full absolute z-10'>
-            <img className='h-full w-full' src="/hero-4.jpg" alt="" />
+            <img className='h-full w-full object-cover' src="/hero-4.jpg" alt="" />
         </div>
         <Navbar/>
     </main>
