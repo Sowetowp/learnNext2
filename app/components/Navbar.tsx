b6e57880007d63d6b49bf2c4051ba0707492c6c9
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 06:33:36 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -49,17 +49,17 @@ const Navbar = () => {
                     /* From Uiverse.io by Shubh0408 */
                         <label>
                             <div
-                                class="w-9 h-10 cursor-pointer flex flex-col items-center justify-center"
+                                className="w-9 h-10 cursor-pointer flex flex-col items-center justify-center"
                             >
-                                <input class="hidden peer" type="checkbox" />
+                                <input className="hidden peer" type="checkbox" />
                                 <div
-                                    class="w-[50%] h-[2px] bg-black rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] peer-checked:rotate-[-45deg]"
+                                    className="w-[50%] h-[2px] bg-black rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] peer-checked:rotate-[-45deg]"
                                 ></div>
                                 <div
-                                    class="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-center peer-checked:hidden"
+                                    className="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-center peer-checked:hidden"
                                 ></div>
                                 <div
-                                    class="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-left -translate-y-[0.45rem] peer-checked:rotate-[45deg]"
+                                    className="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-left -translate-y-[0.45rem] peer-checked:rotate-[45deg]"
                                 ></div>
                             </div>
                         </label>
