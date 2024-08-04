f25ecec95bc7cd5a9591a3caa1c7042010216140
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 05:07:33 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -26,6 +26,7 @@ const Carousel = () => {
     }
 
     const handlePrev = () => {
+        setNext(true)
         setCurrentIndex((prevIndex) =>
             prevIndex === 0 ? slides.length - 1 : prevIndex - 1
         );
