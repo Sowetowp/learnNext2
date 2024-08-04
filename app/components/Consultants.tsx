47e67ab2967e464b68f271b2a84a5dc5ecfd2dec
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 20:54:16 +0100

    auto-commit

--- app/components/Consultants.tsx
+++ app/components/Consultants.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 const Consultants = () => {
     const exp = [
 {img: "/img-10.jpg", name: "JAMES WILLAMSON", post:"FOUNDER"},
-{img: "/img-13.jpg", name: "LISA RAY", post:"DESIGNER"}
+{img: "/img-13.jpg", name: "LISA RAY", post:"DESIGNER"},
+
     ]
     return (
         <>
