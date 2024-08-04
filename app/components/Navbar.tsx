4c7b39a0ed7fb419d224b45f32e2d6227ccba6a4
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 19:46:06 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -40,7 +40,7 @@ const Navbar = () => {
     return (
         <>
             <header className={`w-full transition-colors duration-500 ${scrollHeader ? "fixed z-[9999] bg-white shadow" : "relative z-[9999] md:bg-transparent"}`}>
-                <section className='w-full m-auto max-w-7xl py-4 md:py-7 px-1 md:px-8 xl:px-16 flex items-center justify-between'>
+                <section className='w-full m-auto max-w-7xl py-4 md:py-7 px-2 md:px-8 xl:px-16 flex items-center justify-between'>
                     <h1 className={`text-xl leading-none tracking-widest ${scrollHeader?"text-black":"text-white"}`}>SPERE</h1>
                     <div className='md:flex gap-8 items-center hidden'>
                         <a className={`${scrollHeader?"text-black":"text-white"} text-[0.8rem] font-semibold`}>HOME</a>
