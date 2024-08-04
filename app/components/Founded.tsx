4c244abdfa2d9e32e411842951cb295576002a8d
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 16:04:02 +0100

    auto-commit

--- app/components/Founded.tsx
+++ app/components/Founded.tsx
@@ -5,7 +5,7 @@ const Founded = () => {
         <>
             <section className='w-full max-w-7xl m-auto bg-stone-50 py-28'>
                 <div className='w-1/2'>
-                    <h2>Founded with the goal to serve expertise</h2>
+                    <h2 className='font-'>Founded with the goal to serve expertise</h2>
                     <p>- Template is easy to setup and run</p>
                     <p>- Free support and lifetime updates</p>
                     <p>- Modern and unique design</p>
