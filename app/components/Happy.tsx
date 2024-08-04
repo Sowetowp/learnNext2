d69e00ed1a6034e676d6f179bbb974b972acb1ce
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 21:29:02 +0100

    auto-commit

--- app/components/Happy.tsx
+++ app/components/Happy.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Happy = () => {
   return (
     <>
-    <section className='w-full max-w-7xl bg-stone-50'>
+    <section className='w-full max-w-7xl bg-stone-50 px-16 py-5'>
 
     </section>
     </>
