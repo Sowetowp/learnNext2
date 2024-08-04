5b17baf0f2b0704d44d408fdb1c0c09b7ee882df
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 14:38:58 +0100

    auto-commit

--- app/components/Expert.tsx
+++ app/components/Expert.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Expert = () => {
     return (
         <>
-            <section>
+            <section className='w-full maxw'>
                 <h2>We are an expert consulting firm.</h2>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nemo libero debitis vitae. Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.</p>
             </section>
