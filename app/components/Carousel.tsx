f6fe3a182035d3eacdf6f674e5fe00feb335e92e
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 02:16:45 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -7,6 +7,9 @@ const Carousel = () => {
     <main className='h-screen bg-black w-full max-w-7xl relative'>
         <div className='h-full w-full absolute z-10'>
             <div className='h-full w-full absolute bg-[#00000091] flex justify-center items-center'>
+                <span>
+                    
+                </span>
                 <p className='text-white font-bold text-5xl'>Talented Consultants</p>
                 <p>With over 20 years experience helping business to find comprehensive solutions</p>
                 <a href="">OUR SERVICES</a>
