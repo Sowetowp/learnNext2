f860705a2b2a17daa4e2066bf94b0eafe8207edc
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 16:14:53 +0100

    auto-commit

--- app/components/Founded.tsx
+++ app/components/Founded.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Founded = () => {
     return (
         <>
-            <section className='w-full flex items-center max-w-7xl m-auto bg-stone-50 px-16 py-28'>
+            <section className='w-full flex items-center flex-wrap max-w-7xl m-auto bg-stone-50 px-16 py-28'>
                 <div className='w-1/2'>
                     <h2 className='font-bold w-[80%] text-[1.9rem] mb-6'>Founded with the goal to serve expertise</h2>
                     <p className='text-[1.1rem] border-b pb-1 w-fit font-light text-gray-500 mt-1'>- Template is easy to setup and run</p>
