d135dd86b7e4a661db75f5af6f12c14ac7200a28
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 04:09:32 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -38,7 +38,7 @@ const Carousel = () => {
                 ))}
                 <Navbar />
             </main>
-            <button className='border bg-black' onClick={() => setCurrentIndex(currentIndex + 1)}>kk</button>
+            <button className='border bg-black' onClick={nex}>kk</button>
         </>
     )
 }
