d9e7e55f2435b98b49a0eec22a258712eaaa064c
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 22:42:19 +0100

    auto-commit

--- app/components/Happy.tsx
+++ app/components/Happy.tsx
@@ -32,7 +32,7 @@ const Happy = () => {
     useEffect(() => {
         if (typeof window !== 'undefined') {
             const handleScroll = () => {
-                setScrollHeader(window.pageYOffset > 3000);
+                setScrollHeader(window.pageYOffset > 4000);
             };
             const debouncedHandleScroll = debounce(handleScroll, 50);
             window.addEventListener('scroll', debouncedHandleScroll);
