ec3cb61c7f380e8b9b56f3c74fb770d22e422317
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 20:48:49 +0100

    auto-commit

--- app/components/Consultants.tsx
+++ app/components/Consultants.tsx
@@ -20,7 +20,7 @@ const Consultants = () => {
                             <img className='w-full h-[19rem] object-cover' src="/img-10.jpg" alt="" />
                         </div>
                         <p className='font-medium text-xs tracking-wider mt-5 text-center'>JAMES WILLAMSON</p>
-                        <p>Founder</p>
+                        <p className='text-xs '>Founder</p>
                     </div>
                 </div>
             </section>
