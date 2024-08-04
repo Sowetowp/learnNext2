64dbbe32cc0fa51f65a9f4051738fc8c972af565
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 06:34:09 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -48,8 +48,7 @@ const Navbar = () => {
                     <div>
                     /* From Uiverse.io by Shubh0408 */
                         <label>
-                            <div className="w-9 h-10 cursor-pointer flex flex-col items-center justify-center"
-                            >
+                            <div className="w-9 h-10 cursor-pointer flex flex-col items-center justify-center">
                                 <input className="hidden peer" type="checkbox" />
                                 <div
                                     className="w-[50%] h-[2px] bg-black rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] peer-checked:rotate-[-45deg]"
