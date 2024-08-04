83a286554f6b2c400bbbfccfbaea98088af3e99e
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 01:45:14 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -4,8 +4,8 @@ import Navbar from './Navbar'
 const Carousel = () => {
   return (
     <>
-    <main className='h-screen bg-black w-full max-w-7xl'>
-        <div className='h-full w-full'>
+    <main className='h-screen bg-black w-full max-w-7xl relative'>
+        <div className='h-full w-full absolute'>
             <img className='h-full w-full' src="/hero-4.jpg" alt="" />
         </div>
         <Navbar/>
