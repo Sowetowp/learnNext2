58cb73d216f64621e0bf1c34909992e7c7a9d0a0
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 15:57:24 +0100

    auto-commit

--- app/components/Founded.tsx
+++ app/components/Founded.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Founded = () => {
   return (
     <>
-    <section className='w-full max-w-7xl m-auto bg-gray-50 py-28'></section>
+    <section className='w-full max-w-7xl m-auto bg-brown-50 py-28'></section>
     </>
   )
 }
