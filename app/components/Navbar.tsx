1a505599bb7420dcd94d6dfef64cc030629ef18e
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 06:34:45 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -52,8 +52,7 @@ const Navbar = () => {
                                 <input className="hidden peer" type="checkbox" />
                                 <div className="w-[50%] h-[2px] bg-black rounded-sm transition-all duration-300 origin-left translate-y-[0.45rem] peer-checked:rotate-[-45deg]" ></div>
                                 <div className="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-center peer-checked:hidden" ></div>
-                                <div
-                                    className="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-left -translate-y-[0.45rem] peer-checked:rotate-[45deg]"
+                                <div className="w-[50%] h-[2px] bg-black rounded-md transition-all duration-300 origin-left -translate-y-[0.45rem] peer-checked:rotate-[45deg]"
                                 ></div>
                             </div>
                         </label>
