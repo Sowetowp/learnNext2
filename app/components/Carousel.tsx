89f3d12863a022e013ec5623934c0ce9586a92ba
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 04:33:01 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -17,7 +17,7 @@ const Carousel = () => {
             return "100%"
         }else if(index === slides.length-1 && currentIndex === 0 ){
             return "-100%"
-        }else if(currentIndex === slides.length-1 && index === slides.length-1 ){
+        }else if(currentIndex === slides.length-1 && index === slides.length-2 ){
             return "100%"
         }else if(index === slides.length-1 && currentIndex === 0 ){
             return "-100%"
