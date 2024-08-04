38fa9c4272ac86692a9e3e37fd254522d0b74ee9
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 19:24:46 +0100

    auto-commit

--- app/components/Award.tsx
+++ app/components/Award.tsx
@@ -5,7 +5,7 @@ const Award = () => {
     <>
       <section className='w-full max-w-7xl m-auto'>
         <div className="parallax">
-          <p className='text-white h-[20rem] items-center flex justify-cente font-extrabold text-[3rem]'>We are award winning business consultant</p>
+          <p className='text-white h-[20rem] items-center flex justify-cente font-bold text-[3rem]'>We are award winning business consultant</p>
         </div>
         <div>kkk</div>
       </section>
