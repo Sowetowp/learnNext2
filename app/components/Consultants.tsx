05461904d58b21aaafe150936f783666940f3b3a
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 20:19:31 +0100

    auto-commit

--- app/components/Consultants.tsx
+++ app/components/Consultants.tsx
@@ -11,7 +11,9 @@ const Consultants = () => {
                 <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text.</p>
                 <div className='w-full px-10 flex flex-wrap'>
                     <div className='xl:w-1/4 md:w-1/2 w-full text-center mt-14 px-3'>
-
+<div>
+    
+</div>
                     </div>
                 </div>
             </section>
