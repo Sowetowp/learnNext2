d030146856f7aad8d71a32e0194dc41bf510a788
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 05:10:00 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 
 const Carousel = () => {
     const [currentIndex, setCurrentIndex] = useState(0)
-    const [next, setNext] = useState(false)
+    const [next, setNext] = useState<boolean>(null)
     const slides = [
         '/hero-4.jpg',
         '/hero-5.jpeg',
