152dfe69b17038d4a9b6f057fa5bc8e377563a49
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 02:45:46 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -1,58 +1,24 @@
-"use client";
-import React, { useState } from 'react';
+import React from 'react'
+import Navbar from './Navbar'
 
-const Carousel: React.FC = () => {
-  const slides = [
-    '/hero-4.jpg',
-    '/hero-5.jpeg',
-    '/hero-6.jpg',
-  ];
+const Carousel = () => {
+    return (
+        <>
+            <main className='h-screen bg-black w-full max-w-7xl relative'>
+                <div className='h-full w-full absolute z-10'>
+                    <div className='h-full w-full absolute text-center bg-[#00000091] flex justify-center items-center'>
+                        <span className='pt-20'>
+                            <p className='text-white font-bold text-5xl'>Talented Consultants</p>
+                            <p className='text-white mt-3'>With over 20 years experience helping business to find comprehensive solutions</p>
+                            <a href="" className='mt-4 px-5 py-3 bg-blue-700 inline-block text-xs text-white font-semibold'>OUR SERVICES</a>
+                        </span>
+                    </div>
+                    <img className='h-full w-full object-cover' src="/hero-4.jpg" alt="" />
+                </div>
+                <Navbar />
+            </main>
+        </>
+    )
+}
 
-  // Extend slides array to enable infinite scrolling effect
-  const extendedSlides = [...slides, ...slides, ...slides];
-
-  const [currentIndex, setCurrentIndex] = useState(slides.length);
-
-  const handlePrev = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? extendedSlides.length - slides.length : prevIndex - 1
-    );
-  };
-
-  const handleNext = () => {
-    setCurrentIndex((prevIndex) =>
-      prevIndex === extendedSlides.length - 1 ? slides.length : prevIndex + 1
-    );
-  };
-
-  return (
-    <div className="relative w-full max-w-3xl mx-auto">
-      <div className="overflow-hidden">
-        <div
-          className="flex transition-transform duration-500"
-          style={{ transform: `translateX(-${(currentIndex - slides.length) * (100 / slides.length)}%)` }}
-        >
-          {extendedSlides.map((slide, index) => (
-            <div key={index} className="flex-shrink-0 w-full">
-              <img src={slide} alt={`Slide ${index + 1}`} className="w-full" />
-            </div>
-          ))}
-        </div>
-      </div>
-      <button
-        onClick={handlePrev}
-        className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-md"
-      >
-        &lt;
-      </button>
-      <button
-        onClick={handleNext}
-        className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white p-2 rounded-full shadow-md"
-      >
-        &gt;
-      </button>
-    </div>
-  );
-};
-
-export default Carousel;
+export default Carousel
