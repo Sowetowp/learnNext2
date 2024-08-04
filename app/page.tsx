415852ccfd8baafb8ec92887986cb660b6bb023c
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 13:40:08 +0100

    auto-commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import Carousel from "./components/Carousel";
+import Expert from "./components/Expert";
 
 export default function Home() {
   return (
     <>
       <Carousel />
-      <Ex
+      <Expert/>
     </>
   );
 }
