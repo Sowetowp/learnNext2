4bdefed1badd098163c1253871987d2559ab170f
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 03:26:34 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -38,7 +38,7 @@ const Carousel = () => {
                 <img src={slides[0]} className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="..."/>
             </div>
             <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg" className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="..."/>
+                <img src={slides[1]} className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="..."/>
             </div>
             <div className="hidden duration-700 ease-in-out" data-carousel-item>
                 <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg" className="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2" alt="..."/>
