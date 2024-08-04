2a76f95210d4160273e9ccd58d780c8ff85413db
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 03:46:17 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -11,17 +11,17 @@ const Carousel = () => {
     return (
         <>
             <main className='h-screen bg-black w-full max-w-7xl relative'>
-                {slides.map((slide, index)=>(
-                <div className='h-full w-full absolute z-10'>
-                    <div className='h-full w-full absolute text-center bg-[#00000091] flex justify-center items-center'>
-                        <span className='pt-20'>
-                            <p className='text-white font-bold text-5xl'>Talented Consultants</p>
-                            <p className='text-white mt-3'>With over 20 years experience helping business to find comprehensive solutions</p>
-                            <a href="" className='mt-4 px-5 py-3 bg-blue-700 inline-block text-xs text-white font-semibold'>OUR SERVICES</a>
-                        </span>
+                {slides.map((slide, index) => (
+                    <div key={index} className='h-full w-full absolute z-10'>
+                        <div className='h-full w-full absolute text-center bg-[#00000091] flex justify-center items-center'>
+                            <span className='pt-20'>
+                                <p className='text-white font-bold text-5xl'>Talented Consultants</p>
+                                <p className='text-white mt-3'>With over 20 years experience helping business to find comprehensive solutions</p>
+                                <a href="" className='mt-4 px-5 py-3 bg-blue-700 inline-block text-xs text-white font-semibold'>OUR SERVICES</a>
+                            </span>
+                        </div>
+                        <img className='h-full w-full object-cover' src={slide} alt="" />
                     </div>
-                    <img className='h-full w-full object-cover' src="/hero-4.jpg" alt="" />
-                </div>
                 ))}
                 <Navbar />
             </main>
