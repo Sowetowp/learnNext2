6ab0ba39c9c333196eb99a8b94323ea5bf2d09a5
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-04 03:43:56 +0100

    auto-commit

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './Navbar'
 
 const Carousel = () => {
+    
     return (
         <>
             <main className='h-screen bg-black w-full max-w-7xl relative'>
