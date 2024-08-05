687ddd8226ee98a840c0427b33048a693d71d614
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-05 23:51:52 +0100

    auto-commit

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,24 +1,19 @@
 "use client";
 import React, { useEffect } from 'react';
-import maplibregl from 'maplibre-gl';
-import 'maplibre-gl/dist/maplibre-gl.css';
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
 
 const Contact: React.FC = () => {
     useEffect(() => {
-        const map = new maplibregl.Map({
-            container: 'map', // Container ID
-            style: 'https://demotiles.maplibre.org/style.json', // Map style
-            center: [3.3421, 6.5965], // Starting position [lng, lat]
-            zoom: 13, // Starting zoom
-        });
+        // Ensure Leaflet's CSS is included
+        const link = document.createElement('link');
+        link.rel = 'stylesheet';
+        link.href = 'https://unpkg.com/leaflet@1.7.1/dist/leaflet.css';
+        document.head.appendChild(link);
 
-        // Add a marker
-        new maplibregl.Marker()
-            .setLngLat([3.3421, 6.5965])
-            .setPopup(new maplibregl.Popup().setText('Na here i dey!!'))
-            .addTo(map);
-
-        return () => map.remove();
+        return () => {
+            document.head.removeChild(link);
+        };
     }, []);
 
     return (
@@ -26,7 +21,17 @@ const Contact: React.FC = () => {
             <h2 className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]'>Get in touch with us</h2>
             <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center'>Lorem Ipsum is simply dummy text of the printing and typesetting industry standard.</p>
             <div className='w-full px-10'>
-                <div id="map" className='md:w-1/2 w-full px-3 h-96'></div>
+                <div className='md:w-1/2 w-full px-3'>
+                    <MapContainer center={[6.5965, 3.3421]} zoom={13} className='w-full h-96'>
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        />
+                        <Marker position={[6.5965, 3.3421]}>
+                            <Popup>Na here i dey!!</Popup>
+                        </Marker>
+                    </MapContainer>
+                </div>
                 <div className='md:w-1/2 w-full px-3'></div>
             </div>
         </section>
