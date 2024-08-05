0ca1f605f75373c8f7fba669c9321b23e29de48a
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-05 20:53:39 +0100

    auto-commit

--- app/components/CaseStudies.tsx
+++ app/components/CaseStudies.tsx
@@ -21,7 +21,7 @@ const CaseStudies = () => {
           </div>
         </div>
         <div className='hidden fixe top-0 w-full h-screen bg-[#0000003a] z-[99999]'>
-          <div>
+          <div className='flex items-center justify-center'>
 
           <img className='w-full md:w-[50%]' src="/img_2.jpg" alt="" />
           </div>
