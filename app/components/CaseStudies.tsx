8458c9246c8cf10e14915e8b5eeb3e0b732ca02f
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-05 20:37:42 +0100

    auto-commit

--- app/components/CaseStudies.tsx
+++ app/components/CaseStudies.tsx
@@ -14,7 +14,7 @@ const CaseStudies = () => {
                 </div>
                 <div className='w-full px-10 flex flex-wrap'>
                     <div className='xl:w-1/3 md:w-1/2 w-full mt-14 px-3'>
-                      <div className='w-full h-72'>
+                      <div className='w-full h-64'>
                         <img className='w-full h-full' src="/img_2.jpg" alt="" />
                       </div>
                     </div>
