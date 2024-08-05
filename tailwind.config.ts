10843e805e855d8b0089e5e39a81c4f6e0973173
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-05 22:01:19 +0100

    auto-commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        scroll: {
+          '0%': { transform: 'translateX(0)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+      },
+      animation: {
+        scroll: 'scroll 10s linear infinite',
+      },
     },
   },
   plugins: [],
