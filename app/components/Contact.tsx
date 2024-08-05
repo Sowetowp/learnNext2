f6d4a631403ec5c1435baf4b4d00f67829be9970
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-06 00:00:19 +0100

    auto-commit

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,21 +1,22 @@
 "use client";
-import React, { useEffect } from 'react';
-import maplibregl from 'maplibre-gl';
+import React from 'react';
+import MapLibreGL from 'maplibre-gl';
 import 'maplibre-gl/dist/maplibre-gl.css';
 
 const Contact: React.FC = () => {
-    useEffect(() => {
-        const map = new maplibregl.Map({
+    React.useEffect(() => {
+        const map = new MapLibreGL.Map({
             container: 'map', // Container ID
-            style: 'https://demotiles.maplibre.org/style.json', // Map style
+            style: 'https://demotiles.maplibre.org/style.json', // Map style URL
             center: [3.3421, 6.5965], // Starting position [lng, lat]
-            zoom: 13, // Starting zoom
+            zoom: 13, // Starting zoom level
+            attributionControl: false,
         });
 
         // Add a marker
-        new maplibregl.Marker()
+        new MapLibreGL.Marker()
             .setLngLat([3.3421, 6.5965])
-            .setPopup(new maplibregl.Popup().setText('Na here i dey!!'))
+            .setPopup(new MapLibreGL.Popup().setText('Na here i dey!!'))
             .addTo(map);
 
         return () => map.remove();
@@ -26,11 +27,13 @@ const Contact: React.FC = () => {
             <h2 className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]'>Get in touch with us</h2>
             <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center'>Lorem Ipsum is simply dummy text of the printing and typesetting industry standard.</p>
             <div className='w-full px-10'>
-                <div id="map" className='md:w-1/2 w-full px-3 h-96'></div>
+                <div className='md:w-1/2 w-full px-3 h-96'>
+                    <div id='map' className='h-full w-full'></div>
+                </div>
                 <div className='md:w-1/2 w-full px-3'></div>
             </div>
         </section>
     );
-}
+};
 
 export default Contact;
