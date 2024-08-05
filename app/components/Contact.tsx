5b05b818b78cc840bcb067b9107f783cb5eee59c
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-05 23:34:48 +0100

    auto-commit

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-import L from 'leaflet';
+import * as L from 'leaflet';
 import { useEffect } from 'react';
 
 const icon = L.icon({
