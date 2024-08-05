3dad9027e6f6ced52a2c1750522c66238cef95c0
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-05 20:48:35 +0100

    auto-commit

--- app/components/CaseStudies.tsx
+++ app/components/CaseStudies.tsx
@@ -20,7 +20,7 @@ const CaseStudies = () => {
             </div>
           </div>
         </div>
-        <div className='fixed top-0 w-full h-screen bg-[black]'>
+        <div className='fixed top-0 w-full h-screen bg-[#0000003a] '>
           <img src="/img_2.jpg" alt="" />
         </div>
       </section>
