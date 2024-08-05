822cab88805b3bfd00edcc0dcc54bf3b5321dcb7
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-05 21:48:57 +0100

    auto-commit

--- app/components/Testimony.tsx
+++ app/components/Testimony.tsx
@@ -13,7 +13,7 @@ const Testimony = () => {
                 <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.</p>
                 <div className='w-full px-10 flex flex-wrap'>
                     <div className='xl:w-1/3 md:w-1/2 w-full mt-14 px-3'>
-                        <div className='bg-white p-5 shadow'>
+                        <div className='bg-white p-5 shadow text-center'>
                             <img src="" alt="" />
                             <p className='font-medium text-xs tracking-wider mt-5 text-center'>JAMES WILLAMSON</p>
                             <p className='text-[0.8rem] mt-1 text-gray-500'>CREATIVE DIRECTOR</p>
