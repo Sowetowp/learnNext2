32ede380d319ef33f5a63ad1e3ff0f400c1b1c0c
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-05 22:39:35 +0100

    auto-commit

--- app/components/Testimony.tsx
+++ app/components/Testimony.tsx
@@ -29,7 +29,7 @@ const Testimony = () => {
                         </div>
                     ))}
                 </div>
-                <div className='w-full pt-28 flex justify-around'>
+                <div className='w-full pt-28 flex flex-wrap justify-around'>
                     {sponsors.map((sp, index) => (
                         <img className='w-28' key={index} src={sp.img} alt="" />
                     ))}
