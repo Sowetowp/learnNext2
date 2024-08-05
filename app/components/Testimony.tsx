d32e43a6dd3cd5b5b4b9d2b0dada6bb46a0db722
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-05 21:48:09 +0100

    auto-commit

--- app/components/Testimony.tsx
+++ app/components/Testimony.tsx
@@ -4,10 +4,7 @@ const Testimony = () => {
     const works = [
         { img: "/img_5.jpg" },
         { img: "/img_4.jpg" },
-        { img: "/img_6.jpg" },
-        { img: "/img_7.jpg" },
-        { img: "/img_10.jpg" },
-        { img: "/img_12.jpg" }
+        { img: "/img_6.jpg" }
       ]
     return (
         <>
