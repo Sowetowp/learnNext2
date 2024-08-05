b7f3c4578b9993d43e0757b203efe574bc37f91f
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-05 15:36:21 +0100

    auto-commit

--- app/components/CaseStudies.tsx
+++ app/components/CaseStudies.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CaseStudies = () => {
   return (
     <>
-            <section className='w-full max-w-7xl m-auto bg-white py-28'>
+            <section className='w-full max-w-7xl m-auto bg-white py-24'>
                 <h2 className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]'>Our case studies</h2>
                 <p className='mt-7 m-auto text-[0.9rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
                 <div className='flex flex-wrap mt-11 justify-center items-center gap-5'>
