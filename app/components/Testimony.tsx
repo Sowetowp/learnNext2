bd54f7cdfdc4eb621e12506c00037fcc526a6e2e
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-05 22:42:39 +0100

    auto-commit

--- app/components/Testimony.tsx
+++ app/components/Testimony.tsx
@@ -17,7 +17,7 @@ const Testimony = () => {
             <section className='w-full max-w-7xl m-auto bg-stone-50 py-28'>
                 <h2 className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]'>Our client's testimonials</h2>
                 <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.</p>
-                <div className='w-full px-10 flex flex-wrap'>
+                <div className='w-full md:px-10 px-5 flex flex-wrap'>
                     {works.map((test, index) => (
                         <div key={index} className='xl:w-1/3 md:w-1/2 w-full mt-14 md:px-5'>
                             <div className='bg-white md:p-16 p-8 shadow text-center'>
