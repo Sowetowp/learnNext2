0411c9c5b7dcff8e019cc5aa4ad2dff3cd2298f4
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-05 15:32:41 +0100

    auto-commit

--- app/components/CaseStudies.tsx
+++ app/components/CaseStudies.tsx
@@ -6,7 +6,7 @@ const CaseStudies = () => {
             <section className='w-full max-w-7xl m-auto bg-white py-28'>
                 <h2 className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]'>Our case studies</h2>
                 <p className='mt-7 m-auto text-[0.9rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.</p>
-                <div className='flex justify-center items-center gap-4'>
+                <div className='flex mt-5 justify-center items-center gap-4'>
                     <a className='font-medium text-xs text-gray-600' href="">ALL</a>
                     <a href="">LOGO</a>
                     <a href="">GRAPHIC</a>
