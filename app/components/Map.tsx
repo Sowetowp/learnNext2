aa81916a40266dc6a7b8868a9e3dba135056e329
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-06 16:22:10 +0100

    auto-commit

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -27,6 +27,7 @@ const Map = (Map: MapProps) => {
             style={{ height: "100%", width: "100%" }}
         >
             <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
             <Marker position={posix} draggable={false}>
