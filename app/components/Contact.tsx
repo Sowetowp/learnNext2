4ea1438691f5ae95a5005d5b21b6d4638fdbce6c
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-06 18:25:39 +0100

    auto-commit

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -30,7 +30,7 @@ const Contact = () => {
     ]
     return (
         <>
-            <section className='w-full max-w-7xl m-auto bg-stone-50 py-28'>
+            <section id="sec8" className='w-full max-w-7xl m-auto bg-stone-50 py-28'>
                 <h2 className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]'>Get in touch with us</h2>
                 <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center'>Lorem Ipsum is simply dummy text of the printing and typesetting industry standard.</p>
                 <div className='w-full px-10 flex flex-wrap'>
