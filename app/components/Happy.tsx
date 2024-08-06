26ddb946c44d30f307c59eb5be5e38fad14c3132
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-06 23:46:16 +0100

    auto-commit

--- app/components/Happy.tsx
+++ app/components/Happy.tsx
@@ -82,7 +82,7 @@ const Happy: React.FC<CarouselProps> = ({view}) => {
     ]
     return (
         <>
-            <section className='w-full max-w-7xl flex items-center flex-wrap bg-stone-50 py-4 px-16'>
+            <section id='happy' className='w-full max-w-7xl flex items-center flex-wrap bg-stone-50 py-4 px-16'>
                 {details.map((det, index) => (
                     <div key={index} className='xl:w-1/4 md:w-1/2 w-full text-center py-8'>
                         {det.img}
