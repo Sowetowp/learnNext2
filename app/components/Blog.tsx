3459f027ce559be09ba6a62071931c177795e1c4
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-06 17:07:38 +0100

    auto-commit

--- app/components/Blog.tsx
+++ app/components/Blog.tsx
@@ -13,7 +13,7 @@ const Blog = () => {
                 <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center'>There are many variations of passages of Lorem Ipsum but the majority have suffered in some form.</p>
                 <div className='w-full md:px-10 flex flex-wrap'>
                     {works.map((work, index) => (
-                        <div key={index} className='xl:w-1/3 cursor-pointer md:w-1/3 w-full mt-14 px-4'>
+                        <div key={index} className='xl:w-1/3 cursor-pointer md:w-1/2 w-full mt-14 px-4'>
                             <div className='w-full h-60 relative'>
                                 <p className='bg-blue-700 w-fit bottom-0 px-4 absolute py-2 text-xs text-white'>JAN 11</p>
                                 <img className='w-full object-cover h-full' src={work.img} alt="" />
