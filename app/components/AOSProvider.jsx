2fc92306bbc1c30bf85a76155066e966381a314d
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-07 23:55:57 +0100

    auto-commit

--- app/components/AOSProvider.jsx
+++ app/components/AOSProvider.jsx
@@ -3,7 +3,7 @@ import { useEffect } from 'react'
 import AOS from "aos";
 import "aos/dist/aos.css";
 
-export const AOSInit = () => {
+export const AOSProvider = () => {
     useEffect(() => {
         AOS.init({
             easing: 'ease-out-quad',
