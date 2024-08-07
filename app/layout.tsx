bc78547fb97d8572552f7ff055e6270322466f37
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-07 23:58:51 +0100

    auto-commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -16,9 +17,8 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={inter.className}>
-        <AOSProvider
-        {children}</body>
+      <AOSProvider/>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
