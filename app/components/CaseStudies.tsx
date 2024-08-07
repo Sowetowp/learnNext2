7c0b40350f91d7453403399c74c9d67570e7fed8
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-07 20:42:06 +0100

    auto-commit

--- app/components/CaseStudies.tsx
+++ app/components/CaseStudies.tsx
@@ -13,7 +13,7 @@ const CaseStudies = () => {
     { img: "/img_12.jpg", id: "3" }
   ]
   const filter = (id: number) => {
-    let filtered: Object = worksData.filter((fil) => fil.id === id)
+    let filtered: Object = worksData.filter((fil:Object) => fil.id === id)
     setWorks(filtered)
   }
   return (
