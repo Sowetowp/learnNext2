de4517bd1fc06289724fb1750562e3756073a329
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 00:19:09 +0100

    auto-commit

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -29,7 +29,7 @@ const Services = () => {
                         <div key={index} className='xl:w-1/4 md:w-1/2 w-full md:px-10 text-center mt-14'>
                             <span data-aos="fade-up" data-aos-duration="1000" className='bg-blue-700 inline-block p-4'>{ex.img}</span>
                             <p data-aos="fade-up" data-aos-duration="1000" className='font-medium text-[0.9rem] mt-4'>{ex.head}</p>
-                            <p className='text-sm text-gray-500 mt-3'>Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been standard .</p>
+                            <p data-aos="fade-up" data-aos-duration="1000" className='text-sm text-gray-500 mt-3'>Lorem Ipsum is simply text of the printing and typesetting industry. Lorem Ipsum has been standard .</p>
                         </div>
                     ))}
                 </div>
