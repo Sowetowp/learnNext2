15c164dd688f38031a281d68f09e4fbc6a212958
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-07 20:45:06 +0100

    auto-commit

--- app/components/CaseStudies.tsx
+++ app/components/CaseStudies.tsx
@@ -1,7 +1,10 @@
 "use client";
 import React, { useState } from 'react'
 
-type Work 
+type Work = {
+  id :number,
+  img: string
+}
 const CaseStudies = () => {
   const [preview, setPreview] = useState("")
   const [works, setWorks] = useState([])
