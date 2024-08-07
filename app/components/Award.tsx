04956563d21a0e0f6b30094b65c6222b1200f159
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 00:11:41 +0100

    auto-commit

--- app/components/Award.tsx
+++ app/components/Award.tsx
@@ -5,7 +5,7 @@ const Award = () => {
     <>
       <section className='w-full max-w-7xl m-auto'>
         <div className="parallax">
-          <p className='text-white h-[20rem] items-center flex justify-center text-center m-auto font-bold md:w-[60%] w-[95%] text-[2rem] md:text-[3.3rem] leading-tight'>We are award winning business consultant</p>
+          <p data-aos="fade" data-aos-duration="1000" className='text-white h-[20rem] items-center flex justify-center text-center m-auto font-bold md:w-[60%] w-[95%] text-[2rem] md:text-[3.3rem] leading-tight'>We are award winning business consultant</p>
         </div>
       </section>
     </>
