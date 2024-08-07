29a677a62b7ccc3612b48c468883dcd0573c95a2
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-07 23:56:54 +0100

    auto-commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <AOSP
+    <AOSProvider/>
     <html lang="en" className="scroll-smooth">
       <body className={inter.className}>{children}</body>
     </html>
