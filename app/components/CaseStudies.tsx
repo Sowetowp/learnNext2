d66bb81bb124a91540b06426c434ef929bbf3a5a
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-07 20:40:13 +0100

    auto-commit

--- app/components/CaseStudies.tsx
+++ app/components/CaseStudies.tsx
@@ -12,7 +12,9 @@ const CaseStudies = () => {
     { img: "/img_10.jpg", id: "2" },
     { img: "/img_12.jpg", id: "3" }
   ]
-  const filter 
+  const filter = ()=>{
+    
+  }
   return (
     <>
       <section id='sec6' className='w-full max-w-7xl m-auto bg-white py-24'>
