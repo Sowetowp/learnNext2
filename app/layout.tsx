8f33ffa60b2ddf2414d39b702ca49187bb2ae53a
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-07 23:58:40 +0100

    auto-commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,8 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="scroll-smooth">
-      <AOSProvider/>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <AOSProvider
+        {children}</body>
     </html>
   );
 }
