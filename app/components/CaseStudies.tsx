4ae258291fd751476c292005938aa3c0d8079e93
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-07 20:37:49 +0100

    auto-commit

--- app/components/CaseStudies.tsx
+++ app/components/CaseStudies.tsx
@@ -4,12 +4,12 @@ import React, { useState } from 'react'
 const CaseStudies = () => {
   const [preview, setPreview] = useState("")
   const [works, setWorks] = useState([
-    { img: "/img_2.jpg" , id:"1" },
-    { img: "/img_4.jpg" , id:"2" },
-    { img: "/img_6.jpg" , id:"3" },
-    { img: "/img_7.jpg" , id:"1" },
-    { img: "/img_10.jpg" , id:"2" },
-    { img: "/img_12.jpg" , id:"3" }
+    { img: "/img_2.jpg", id: "1" },
+    { img: "/img_4.jpg", id: "2" },
+    { img: "/img_6.jpg", id: "3" },
+    { img: "/img_7.jpg", id: "1" },
+    { img: "/img_10.jpg", id: "2" },
+    { img: "/img_12.jpg", id: "3" }
   ])
   return (
     <>
