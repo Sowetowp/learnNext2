68994b0b29a52cfeec891f24f77340de1bc6abea
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 01:56:55 +0100

    auto-commit

--- app/components/Blog.tsx
+++ app/components/Blog.tsx
@@ -5,9 +5,9 @@ const Blog = () => {
         head: "Latest news",
         body: "There are many variations of passages of Lorem Ipsum but the majority have suffered in some form.",
         works: [
-            { img: "/img_9.jpeg", title: "Points to remember in the meeting", det: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text for print." },
-            { img: "/img_10.jpeg", title: "The perfect plan for the business", det: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text for print." },
-            { img: "/img_11.jpeg", title: "Analytics is the important factor", det: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text for print." }
+            { img: "/img_9.jpeg", title: "Points to remember in the meeting", det: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text for print." day: "JAN 11" },
+            { img: "/img_10.jpeg", title: "The perfect plan for the business", det: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text for print." day: "JAN 11" },
+            { img: "/img_11.jpeg", title: "Analytics is the important factor", det: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text for print." day: "JAN 11" }
         ]
     }
     return (
