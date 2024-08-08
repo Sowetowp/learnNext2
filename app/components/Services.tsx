5e3b9abf6abb2b57309b75a42470422605db6c77
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 01:13:49 +0100

    auto-commit

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -33,7 +33,7 @@ const Services = () => {
                         <div key={index} className='xl:w-1/4 md:w-1/2 w-full md:px-10 text-center mt-14'>
                             <span data-aos="fade-up" data-aos-duration="1000" className='bg-blue-700 inline-block p-4'>{ex.img}</span>
                             <p data-aos="fade-up" data-aos-duration="1000" className='font-medium text-[0.9rem] mt-4'>{ex.head}</p>
-                            <p data-aos="fade-up" data-aos-duration="1000" className='text-sm text-gray-500 mt-3'>{ex.}</p>
+                            <p data-aos="fade-up" data-aos-duration="1000" className='text-sm text-gray-500 mt-3'>{ex.body}</p>
                         </div>
                     ))}
                 </div>
