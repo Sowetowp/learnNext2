d393d9c7c28ad8b0de7d3862225cb6310d53f425
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 01:40:46 +0100

    auto-commit

--- app/components/Testimony.tsx
+++ app/components/Testimony.tsx
@@ -15,7 +15,7 @@ const Testimony = () => {
 
     const testimonyData = {
         head: "Our client's testimonials",
-        body: "",
+        body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.",
     }    
     return (
         <>
