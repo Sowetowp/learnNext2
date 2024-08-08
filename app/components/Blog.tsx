f7d1b21667b977d03b28007e39917896cd1d3200
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 01:56:40 +0100

    auto-commit

--- app/components/Blog.tsx
+++ app/components/Blog.tsx
@@ -19,7 +19,7 @@ const Blog = () => {
                     {blogData.works.map((work, index) => (
                         <div data-aos="fade-up" data-aos-duration="1000" key={index} className='xl:w-1/3 m-auto cursor-pointer md:w-1/2 w-full mt-14 px-4'>
                             <div data-aos="fade-up" data-aos-duration="1000" className='w-full h-60 relative'>
-                                <p className='bg-blue-700 w-fit bottom-0 px-4 absolute py-2 text-xs text-white'>{JAN 11}</p>
+                                <p className='bg-blue-700 w-fit bottom-0 px-4 absolute py-2 text-xs text-white'>{work.day}</p>
                                 <img className='w-full object-cover h-full' src={work.img} alt="" />
                             </div>
                             <p data-aos="fade-up" data-aos-duration="1000" className='font-medium text-lg mt-5'>{work.title}</p>
