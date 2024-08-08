e80c368d268e8e777bd8bccb125f9446b5a40294
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 01:00:37 +0100

    auto-commit

--- app/components/Expert.tsx
+++ app/components/Expert.tsx
@@ -23,7 +23,7 @@ const Expert = () => {
         <>
             <section id='sec2' className='w-full max-w-7xl m-auto bg-white py-28'>
                 <h2 className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]' data-aos="fade-up" data-aos-duration="1000">{expertData.head}</h2>
-                <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center' data-aos="fade-up" data-aos-duration="1000">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni nemo libero debitis vitae. Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.Sapiente quos illum ab rem deleniti adipisci suscipit dignissimos.</p>
+                <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center' data-aos="fade-up" data-aos-duration="1000">{expertData.body}</p>
                 <div className='w-full px-10 flex flex-wrap'>
                     {exp.map((ex, index) => (
                         <div key={index} className='md:w-1/3 w-full md:px-6 xl:px-10 text-center mt-14'>
