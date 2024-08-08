2f7d056b2a0911ec8983de87e8814b494833ccbe
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 12:33:42 +0100

    auto-commit

--- app/page.tsx
+++ app/page.tsx
@@ -165,7 +165,7 @@ details: [
       <Award awardData={texts.awardData} />
       <Services servicesData={texts.servicesData}/>
       <Consultants consultantData={texts.consultantData}/>
-      <Happy view={view} details={texts.details}  />
+      <Happy view={view} details={texts.details} setCount={setCount} setCount1={setCount1} setCount2={setCount2} setCount3={setCount3} />
       <CaseStudies />
       <Testimony />
       <Blog />
