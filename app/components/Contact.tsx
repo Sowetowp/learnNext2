724d339245b8ff47ff8a99fd67107899b02d205d
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 01:59:29 +0100

    auto-commit

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -36,7 +36,7 @@ const Contact = () => {
         <>
             <section id="sec8" className='w-full overflow-hidden max-w-7xl m-auto bg-stone-50 py-28'>
                 <h2 className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]' data-aos="fade-up" data-aos-duration="1000">{contactData.head}</h2>
-                <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center' data-aos="fade-up" data-aos-duration="1000">Lorem Ipsum is simply dummy text of the printing and typesetting industry standard.</p>
+                <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center' data-aos="fade-up" data-aos-duration="1000">{contactData.body}</p>
                 <div className='w-full px-10 flex flex-wrap'>
                     {exp.map((ex, index) => (
                         <div key={index} className='md:w-1/3 w-full md:px-6 xl:px-10 text-center mt-14'>
