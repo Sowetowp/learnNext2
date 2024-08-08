9457b2b166ff7b7e5fd7ac034bce43468e698973
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 01:36:19 +0100

    auto-commit

--- app/components/CaseStudies.tsx
+++ app/components/CaseStudies.tsx
@@ -8,7 +8,6 @@ interface Work {
 const CaseStudies = () => {
   const [preview, setPreview] = useState("")
   const [current, setCurrent] = useState(0)
-  const [works, setWorks] = useState<Work[]>(caseData.worksData)
   const filterer = (id: number) => {
     let filtered: Work[] = caseData.worksData.filter((fil: Work) => fil.id === id)
     setWorks(filtered)
@@ -26,6 +25,7 @@ const CaseStudies = () => {
       { img: "/img_12.jpg", id: 3 }
     ]
   }
+  const [works, setWorks] = useState<Work[]>(caseData.worksData)
   return (
     <>
       <section id='sec6' className='w-full max-w-7xl m-auto bg-white py-24'>
