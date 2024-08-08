b9543c6f1d553dae42a4cfe23d6b9a59c36b1beb
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 01:30:10 +0100

    auto-commit

--- app/components/CaseStudies.tsx
+++ app/components/CaseStudies.tsx
@@ -16,14 +16,6 @@ const CaseStudies = () => {
     { img: "/img_10.jpg", id: 2 },
     { img: "/img_12.jpg", id: 3 }
   ])
-  const worksData: Work[] = [
-    { img: "/img_2.jpg", id: 1 },
-    { img: "/img_4.jpg", id: 2 },
-    { img: "/img_6.jpg", id: 3 },
-    { img: "/img_7.jpg", id: 1 },
-    { img: "/img_10.jpg", id: 2 },
-    { img: "/img_12.jpg", id: 3 }
-  ]
   const filterer = (id: number) => {
     let filtered: Work[] = worksData.filter((fil: Work) => fil.id === id)
     setWorks(filtered)
