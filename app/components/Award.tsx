95f1da6bc0c303cc3f9fa42bdcd6b1f23df0648f
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 01:08:32 +0100

    auto-commit

--- app/components/Award.tsx
+++ app/components/Award.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 
 const Award = () => {
-  const awardData = ""
+  const awardData = "We are award winning business consultant"
+  
   return (
     <>
       <section className='w-full max-w-7xl m-auto'>
         <div className="parallax">
-          <p data-aos="fade" data-aos-duration="1000" className='text-white h-[20rem] items-center flex justify-center text-center m-auto font-bold md:w-[60%] w-[95%] text-[2rem] md:text-[3.3rem] leading-tight'>We are award winning business consultant</p>
+          <p data-aos="fade" data-aos-duration="1000" className='text-white h-[20rem] items-center flex justify-center text-center m-auto font-bold md:w-[60%] w-[95%] text-[2rem] md:text-[3.3rem] leading-tight'>{awardData}</p>
         </div>
       </section>
     </>
