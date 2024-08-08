ba13a60633b66e4ea79c828e4a461ff6d6dc4653
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 01:53:56 +0100

    auto-commit

--- app/components/Blog.tsx
+++ app/components/Blog.tsx
@@ -13,7 +13,7 @@ const Blog = () => {
     return (
         <>
             <section id='sec7' className='w-full max-w-7xl m-auto bg-white py-28'>
-                <h2 className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]' data-aos="fade-up" data-aos-duration="1000">{}</h2>
+                <h2 className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]' data-aos="fade-up" data-aos-duration="1000">{blogData.head}</h2>
                 <p className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-2/3 xl:w-1/2 text-center' data-aos="fade-up" data-aos-duration="1000">There are many variations of passages of Lorem Ipsum but the majority have suffered in some form.</p>
                 <div className='w-full md:px-10 flex flex-wrap'>
                     {works.map((work, index) => (
