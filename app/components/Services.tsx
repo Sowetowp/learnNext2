d2fa079a2d905b8ad4faee08c845e7e6adac03fd
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 12:18:27 +0100

    auto-commit

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -10,7 +10,7 @@ const Services: React.FC<servicesProp> = ({servicesData}) => {
                 <h2 data-aos="fade-up" data-aos-duration="1000" className='font-bold text-[1.6rem] text-center m-auto max-w-[80%]'>{servicesData.head}</h2>
                 <p data-aos="fade-up" data-aos-duration="1000" className='mt-7 m-auto text-[0.95rem] text-gray-500 w-[90%] md:w-[60%] xl:w-[40%] text-center'>{servicesData.body}</p>
                 <div className='w-full px-10 flex flex-wrap'>
-                    {servicesData.exp.map((ex:any, index:an)y => (
+                    {servicesData.exp.map((ex:any, index:any) => (
                         <div key={index} className='xl:w-1/4 md:w-1/2 w-full md:px-10 text-center mt-14'>
                             <span data-aos="fade-up" data-aos-duration="1000" className='bg-blue-700 inline-block p-4'>{ex.img}</span>
                             <p data-aos="fade-up" data-aos-duration="1000" className='font-medium text-[0.9rem] mt-4'>{ex.head}</p>
