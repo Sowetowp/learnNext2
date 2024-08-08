32ed12e4783c325cd2dcbef959acb7fa2c204901
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 02:20:17 +0100

    auto-commit

--- app/page.tsx
+++ app/page.tsx
@@ -59,7 +59,7 @@ export default function Home() {
   }
   return (
     <>
-      <Carousel view={view} />
+      <Carousel view={view} slides={} />
       <Expert />
       <Founded />
       <Award />
