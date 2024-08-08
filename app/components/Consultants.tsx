6daf14eb437d753765fbd381908c3c000bc61df4
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 01:18:33 +0100

    auto-commit

--- app/components/Consultants.tsx
+++ app/components/Consultants.tsx
@@ -1,12 +1,6 @@
 import React from 'react'
 
 const Consultants = () => {
-    const exp = [
-        { img: "/img-10.jpg", name: "JAMES WILLAMSON", post: "FOUNDER" },
-        { img: "/img-17.jpg", name: "KATE NELSON", post: "ACCOUNTANT" },
-        { img: "/img-13.jpg", name: "LISA RAY", post: "DESIGNER" },
-        { img: "/img-15.jpg", name: "JOHN DOE", post: "DIRECTOR" }
-    ]
     const consultantData = {
         head:"Our expert consultants",
         body:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s standard dummy text."
