484947ab188a8b79dd6891d84afd991df31bdff7
Abdulmalik <ayodejiamzat@gmail.com>
2024-08-08 02:26:05 +0100

    auto-commit

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -2,7 +2,8 @@
 import React, { useEffect, useState } from 'react'
 
 interface CarouselProps {
-    view: string;
+    view: string,
+    navData: any
 }
 
 const Navbar: React.FC<CarouselProps> = ({ view, navData }) => {
